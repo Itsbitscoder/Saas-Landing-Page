10974d722308de5f1b5584699db9124954721d69
Kunal Jadhav <kunaljadhav2305@gmail.com>
2024-07-22 02:06:27 +0530

    Resolved merge conflicts

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,11 +1,23 @@
+"use client"
 import productImage from '@/assets/product-image.png'
 import pyrimideImage from '@/assets/pyramid.png'
 import tubeImage from '@/assets/tube.png'
 import Image from 'next/image';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+import { constrainedMemory } from 'process';
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end','end start']
+  })
+
+  const translateY =useTransform(scrollYProgress, [0,1], [150, -150])
+
   return (
-    <section className='overflow-x-clip bg-gradient-to-b from-[#FFFF] to-[#D2DCFF] py-24'>
+    <section ref={sectionRef} className='overflow-x-clip bg-gradient-to-b from-[#FFFF] to-[#D2DCFF] py-24'>
       <div className="container">
         <div className='section-heading'>
         <div className='flex justify-center'>
@@ -22,8 +34,8 @@ export const ProductShowcase = () => {
         </div>
         <div className='relative'>
         <Image src={productImage} alt='ProductImg' className='mt-10' />
-        <Image src={pyrimideImage} alt='Pytimide' height={262} width={260} className='absolute -right-36 -top-32 hidden md:block' />
-        <Image src={tubeImage} alt='tube' height={248} className='absolute bottom-24 -left-36 hidden md:block' />
+        <motion.img src={pyrimideImage.src} style={{translateY}} alt='Pytimide' height={262} width={260} className='absolute -right-36 -top-32 hidden md:block' />
+        <motion.img src={tubeImage.src} alt='tube' style={{translateY}} height={248} width={248} className='absolute bottom-24 -left-36 hidden md:block' />
         </div>
       </div>
     </section>
